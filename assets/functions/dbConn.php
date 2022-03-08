6dc7cbe7ecc34a7defb0401a3e0c9fadec3d1217
Stijndus <stijn@ardusit.nl>
2022-03-08 12:17:16 +0100

    rewrote the connection function

--- assets/functions/dbConn.php
+++ assets/functions/dbConn.php
@@ -1,5 +1,4 @@
 <?php
     function dbCon(){
-        $dbh = new PDO('mysql:host=localhost;dbname=to_do_list', 'root', 'mysql');
-        return $dbh;
+       return new PDO('mysql:host=localhost;dbname=to_do_list', 'root', 'mysql');
     }
